71bdec7dfb5279244e0aab81c74020af39ab4f32
saidoud <develop.softoud@gmail.com>
2021-09-22 18:23:59 +0100

    particule animation

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,47 +1,30 @@
 import React from "react";
 import Image from "next/image";
 import { makeStyles } from "@mui/styles";
+import ParticleBackground from "../ui/ParticleBackground";
 
 function Banner() {
   const classes = useStyles();
   return (
     <div className={classes.container}>
       {/* <Image
-        className={classes.backgroundImage}
-        src="/assets/carousel3.jpg"
-        layout="fill"
-      /> */}
-      <Image
         alt="Mountains"
         src="/assets/carousel3-min.jpg"
         layout="fill"
         loading="eager"
-      />
+      /> */}
+      <ParticleBackground />
     </div>
   );
 }
 
 const useStyles = makeStyles({
   container: {
-    // position: "relative",
-    // height: "calc(100vh - 167px)",
-    // width: "100%",
-    // backgroundImage: "url(/assets/carousel3-min.jpg)",
-    // backgroundSize: "cover",
-    // backgroundPosition: "center",
-    // backgroundRepeat: "no-repeat",
-    // backgroundColor: "#cccccc",
+    backgroundColor: "#26ADE4",
     position: "relative",
-    height: "100vh",
+    height: "400px",
     overflow: "hidden",
     zIndex: -1,
   },
-  // backgroundImage: {
-  //   position: "absolute",
-  //   top: "0",
-  //   bottom: "0",
-  //   left: "0",
-  //   right: "0",
-  // },
 });
 export default Banner;
